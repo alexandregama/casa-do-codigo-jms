07bfcfa665eec0af44dc2ce7ed33dcf045a36d41
alexandregama <alexandre.gama.lima@gmail.com>
2015-08-10 22:45:27 -0300

    Adiciona o Tratador de Mensagens de Registra

---
+++ src/br/com/casadocodigo/jms/TratadorDeMensagem.java
@@ -0,0 +1,20 @@
+package br.com.casadocodigo.jms;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+public class TratadorDeMensagem implements MessageListener {
+
+	@Override
+	public void onMessage(Message message) {
+		TextMessage text = (TextMessage) message;
+		try {
+			System.out.println("Tratador recebendo a mensagem: " + text.getText());
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
