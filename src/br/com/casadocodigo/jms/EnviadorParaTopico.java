79cd36043ddf5678d786c799dc39581d0e2a7d41
alexandregama <alexandre.gama.lima@gmail.com>
2015-08-10 23:11:37 -0300

    Cria o Enviador de Mensagens para o Topico

---
+++ src/br/com/casadocodigo/jms/EnviadorParaTopico.java
@@ -0,0 +1,33 @@
+package br.com.casadocodigo.jms;
+
+import java.util.Scanner;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.JMSProducer;
+import javax.jms.Topic;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class EnviadorParaTopico {
+
+	public static void main(String[] args) throws NamingException {
+		InitialContext ic = new InitialContext();
+		ConnectionFactory connection = (ConnectionFactory) ic.lookup("jms/RemoteConnectionFactory");
+		Topic topic = (Topic) ic.lookup("jms/TOPIC.LIVRARIA");
+		
+		try (JMSContext context = connection.createContext("jms", "jms2")) {
+			JMSProducer producer = context.createProducer();
+			
+			System.out.println("Digite para enviar para o Topico: ");
+			Scanner teclado = new Scanner(System.in);
+			while (teclado.hasNextLine()) {
+				String mensagem = teclado.nextLine();
+				
+				producer.send(topic, mensagem);
+			}
+			teclado.close();
+		}
+	}
+	
+}
